0c6a0eaeb8175988f9391ed17adc2da4905d8fe1
Morning <morning@admins-iMac-3.local>
2022-11-28 12:32:27 +0800

    js nov28

---
+++ js/basic/function.js
@@ -0,0 +1,19 @@
+let name = `""jamie""`;
+let myNumber1 = 1;
+let myNumber2 = 3;
+let myNumber3 = myNumber1 + myNumber2;
+myNumber4 = 6;
+console.log(myNumber3);
+console.log(name);
+
+//Prompt hurvuuleh
+let number = "15";
+console.log(typeof number);
+let realNumber = parseInt(number);
+console.log(typeof realNumber);
+
+//dsfg
+let num1 = parseFloat(prompt("Enter first number"));
+let num2 = parseFloat(prompt("Enter second number"));
+alert("2 toonii niilber n: " + (num1 + num2));
+alert("2 toog huvaagad uldegdel n: " + (num1 % num2));
---
+++ js/basic/test.js
@@ -0,0 +1,4 @@
+let num1 = parseFloat(prompt("Enter first number"));
+let num2 = parseFloat(prompt("Enter second number"));
+alert("2 toonii niilber n: " + (num1 + num2));
+alert("2 toog huvaagad uldegdel n: " + (num1 % num2));
