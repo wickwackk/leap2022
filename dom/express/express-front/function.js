46a041efbb653439109f9a4ba8baf3efe2fc285e
Morning <morning@admins-iMac-3.local>
2022-12-28 12:49:55 +0800

    dec28 at class

--- dom/express/express-front/function.js
+++ dom/express/express-front/function.js
@@ -11,6 +11,7 @@ newBigDiv.style.gap = "20px";
 newBigDiv.style.margin = "20px";
 let tr = true;
 button.addEventListener("click", () => {
+  newBigDiv.innerHTML = "";
   if (tr) {
     fetch("http://localhost:3333/api/cars")
       .then((res) => res.json())
@@ -20,7 +21,7 @@ button.addEventListener("click", () => {
           newDiv.style.height = "460px";
           newDiv.style.width = "450px";
           newDiv.innerHTML = `<div class="card" style ="height: 460px">
-        <img src="${car.image}" class="card-img-top" alt="...">
+        <img style="height: 400px" src="${car.image}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${car.model}</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -33,12 +34,75 @@ button.addEventListener("click", () => {
     tr = false;
   } else {
     tr = true;
-    newBigDiv.innerHTML = "";
   }
 });
 
 let modelSelect = document.getElementById("modelSelect");
 let brandSelect = document.getElementById("brandSelect");
 
-modelSelect.addEventListener("change", () => {});
-brandSelect.addEventListener("change", () => {});
+fetch("http://localhost:3333/api/cars")
+  .then((res) => res.json())
+  .then((res) => {
+    let arr = [];
+    res.map((car) => {
+      if (arr.includes(car.brand)) {
+        arr = arr;
+      } else {
+        arr.push(car.brand);
+      }
+    });
+    arr.map((uniqueCar) => {
+      let option = document.createElement("option");
+      option.innerHTML = uniqueCar;
+      option.value = uniqueCar;
+      brandSelect.appendChild(option);
+    });
+  });
+modelSelect.style.display = "none";
+let brandSelected = document.getElementById("brandSelected");
+let modelSelected = document.getElementById("modelSelected");
+
+brandSelect.addEventListener("change", () => {
+  brandSelected.style.display = "none";
+  modelSelect.style.display = "block";
+  modelSelect.innerHTML = "";
+  modelSelect.appendChild(modelSelected);
+  fetch("http://localhost:3333/api/cars")
+    .then((res) => res.json())
+    .then((res) => {
+      res.map((car) => {
+        if (brandSelect.value == car.brand) {
+          let option = document.createElement("option");
+          option.innerHTML = car.model;
+          option.value = car.model;
+          modelSelect.appendChild(option);
+        }
+      });
+    });
+});
+
+let searchBtn = document.getElementById("searchBtn");
+let modal = document.getElementById("exampleModal");
+searchBtn.addEventListener("click", () => {
+  newBigDiv.innerHTML = "";
+  fetch("http://localhost:3333/api/cars")
+    .then((res) => res.json())
+    .then((res) => {
+      res.map((car) => {
+        if (car.brand == brandSelect.value && car.model == modelSelect.value) {
+          let newDiv = document.createElement("div");
+          newDiv.style.height = "460px";
+          newDiv.style.width = "450px";
+          newDiv.innerHTML = `<div class="card" style ="height: 460px">
+        <img style="height: 400px" src="${car.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${car.model}</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-secondary">Go somewhere</a>
+        </div>
+      </div>`;
+          newBigDiv.appendChild(newDiv);
+        }
+      });
+    });
+});
